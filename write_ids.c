904c44f2809ece73e86bbf9a3e6c235e46ac04bf
Cooper Bush <ctb5471@psu.edu>
2024-09-11 17:28:12 -0400

    commit 1

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "ctb5471";
+    char STUDENT_ID[] = "962283061";
 
     return 0 ;
 }
